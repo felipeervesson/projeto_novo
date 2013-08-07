56aa36e2cc32bd6e0bafb3843ec7561580084bd2
Fulano <Fulano@Home>
2013-08-07 20:38:50 -0300

    Adicionando o Projeto Inicial separando css

--- index.php
+++ index.php
@@ -1,148 +1,61 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8" />
-        <title>projeto_novo</title>
-        <style type="text/css">
-             body {
-                    margin:0px;
-                    background-color: #cccccc;
-                    font-family:"Trebuchet MS", Tahoma, Arial, Verdana;
-                    font-size:12px;
-                    color:#000;
-            }
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-type" content="text/html;charset=ISO-8859-1" />
+<title>Exemplo Estrutura de Site Tableless</title>
 
-            #todo {
-                    position: relative;
-                    width: 750px;
-                    border: 1px solid black;
-                    margin:0px auto;
-                    background-color: white;
-            }
+<link rel="stylesheet" type="text/css" href="estilo.css" />
+<script language="javascript" src="script.js"></script>
 
-            #topo {
-                    position: relative;
-                    width: 746px;
-                    height: 100px;
-                    border: 2px solid red;
-            }
+</head>
 
-            #meio {
-                    position: relative;
-                    width: 750px;
-                    min-height: 400px;
-                    background-color: #eeeeee;
-            }
+<body>
 
-            #rodape {
-                    width: 746px;
-                    height: 75px;
-                    border: 2px solid brown;
-            }
-
-           
-
-            #miolo {
-                    float: left;
-                    width: 400px;
-                    min-height: 400px;
-                    border: 2px solid blue;
-            }
-
-            #direita {
-                    float: right;
-                    width: 150px;
-                    min-height: 400px;
-                    border: 2px solid yellow;
-            }
-
-            #menu {
-                    width: 150px;
-                    height: 400px;
-            }
-
-            .itemMenu {
-                    width: 140px;
-                    height: 21px;
-                    background-color: lightyellow;
-                    padding: 3px 0px 0px 10px;
-                    border-bottom: 1px solid black;
-            }
-
-            #logo {
-                    float: left;
-                    width: 200px;
-                    height: 100px;
-                    text-align: center;
-                    background-image: url(img1.GIF);
-                    margin-right: 20px;
-            }
-
-            .secao_miolo {
-                    width: 428px;
-                    height: 150px;
-                    text-align: center;
-                    background-color: #cccccc;
-                    margin: 2px 5px 4px 5px;
-            }
-
-            .secao_direita {
-                    width: 140px;
-                    height: 100px;
-                    text-align: center;
-                    background-color: #cccccc;
-                    margin: 4px 5px 4px 5px;
-            }
-
-            #rodape_direita {
-                    float: right;
-                    width: 200px;
-                    text-align: right;
-                    margin: 5px 10px 0px 0px
-            }
-
-#rodape_direita a{color: #cc6600; text-decoration: none;}
-#rodape_direita a:hover{color: #cc6600; text-decoration: underline;}   
-        </style> 
-    </head>
-    <body>
-      <div id="todo">
+<div id="todo">
 	<div id="topo">
-		<div id="logo"><img src ="img1.gif"></div>
-		MASTERS INFOR
-	</div>  
-        <div id="meio">
+		<div id="logo">Logo</div>
+
+		<p>Masters Infor</p>Grafica e Solucoes em Informatica
+	</div>
+
+	<div id="meio">
+		<div id="esquerda">
+			<div id="menu">
+				<div class="itemMenu">Principal</div>
+				<div class="itemMenu">Empresa</div>
+				<div class="itemMenu">Produtos</div>
+				<div class="itemMenu">Serviços</div>
+				<div class="itemMenu">Contato</div>
+			</div>
+		</div>
+
 		<div id="miolo">
-                     <fieldset class="password">
-                        <legend> Autenticação </legend>
-                        <label for="password">login: </label>
-                        <input type="password" id="login"/></br>
-                        <label for="Password">Senha:</label>
-                        <input type="Password" id="Senha"/><br/><br/> 
-                     </fieldset>
-        <center><div><button name=”submit” type=”submit” onclick ="return validar()">Submeter</button></div></center>
-        <center><div><button name=”submit” type=”submit” onclick ="return validar()">Cadastre Agora</button></div></center>
+			<div class="secao_miolo">Seção 1</div>
+		</div>
+
+		<div id="direita">
+                 <div class="secao_direita">
+                     <form name="input" action=""method="get"> 
+                        Nome<input type="text" nome="user">
+                     <br>
+                        Senha<input type="text" nome="user">
+                     <br>
+                     <input type="submit" value="Login">
+                                </form>
+                        Casso Nao tenha Cadastro
+                
+                
                 </div>
-        </div>
-          <div id="rodape">
-		<div id="rodape_direita">
-			Rua Candido Martins dos Santos - N°603<br/>
-			Parnamirim - RN - CEP: 59150-000<br/>
-			Email: <a href="MastersInfor:suportemi@live.com">suportemi@live.com</a>
+                        <div class="secao_direita">Secao 2</div>
+		</div>
+		<div style="clear: both;"></div>
+	</div>
+	<div id="rodape">
+		<div id="rodape_direita">hfghfghfgh
+			
 		</div>
 	</div>
-    </div>
-    </body>
+</div>
+
+</body>
 </html>
-
-
-
-
-
-
-  
-
-
-
-
-
